5b292d04aa99087a4a367f195476f7af5dbaa9f5
fron-end <105318613+fron-end@users.noreply.github.com>
2022-05-13 00:32:32 +0300

    kubaaa

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/photo/03.svg" alt="arrows"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/photo/02.svg" alt="arrows"></button>',
+    });
+});
